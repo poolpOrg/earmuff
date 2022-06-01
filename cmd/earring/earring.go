2ed0d675a5cbf9298932fd6821f9a2d6f409c01a
Gilles Chehade <gilles@poolp.org>
2022-06-01 09:10:46 +0200

    remove unusd code

--- cmd/earring/earring.go
+++ cmd/earring/earring.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"math"
 	"os"
 	"time"
 
@@ -12,83 +10,6 @@ import (
 	"github.com/faiface/beep/speaker"
 	"github.com/poolpOrg/earring/parser"
 )
-
-func ticker(bpm uint, beats uint, duration uint, done <-chan bool) {
-	step := time.Minute / time.Duration(bpm*(duration/beats))
-	fmt.Println("will tick every", step)
-
-	loTick := LoTick{}
-	hiTick := HiTick{}
-
-	now := time.Now()
-	next := now.Add(step)
-
-	i := uint(0)
-	for {
-		time.Sleep(time.Until(next))
-		// do something
-		if (i % beats) == 0 {
-			go hiTick.Play(step / time.Duration(duration))
-		} else {
-			go loTick.Play(step / time.Duration(duration))
-		}
-		i++
-		select { // check whether `done` was closed
-		case <-done:
-			return
-		default:
-			// pass
-		}
-
-		// next is step - delta between now and previous next (catchup lags)
-		next = next.Add(step - time.Now().Sub(next))
-	}
-}
-
-type LoTick struct{}
-type HiTick struct{}
-
-func (st *LoTick) Play(duration time.Duration) {
-	sr := beep.SampleRate(44100)
-	done := make(chan bool)
-	speaker.Play(beep.Seq(beep.Take(sr.N(duration), st), beep.Callback(func() {
-		done <- true
-	})))
-	<-done
-}
-
-func (st LoTick) Stream(samples [][2]float64) (n int, ok bool) {
-	for i := range samples {
-		sample := math.Sin((math.Pi * 2 / float64(44100)) * 220.0 * float64(i))
-		samples[i][0] = sample
-		samples[i][1] = sample
-	}
-	return len(samples), true
-}
-func (st LoTick) Err() error {
-	return nil
-}
-
-func (st *HiTick) Play(duration time.Duration) {
-	sr := beep.SampleRate(44100)
-	done := make(chan bool)
-	speaker.Play(beep.Seq(beep.Take(sr.N(duration), st), beep.Callback(func() {
-		done <- true
-	})))
-	<-done
-}
-
-func (st HiTick) Stream(samples [][2]float64) (n int, ok bool) {
-	for i := range samples {
-		sample := math.Sin((math.Pi * 2 / float64(44100)) * 440.0 * float64(i))
-		samples[i][0] = sample
-		samples[i][1] = sample
-	}
-	return len(samples), true
-}
-func (st HiTick) Err() error {
-	return nil
-}
 
 func main() {
 	flag.Parse()
